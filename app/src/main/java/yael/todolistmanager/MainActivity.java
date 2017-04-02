5bd4c7e5c8d5ab06135be6c6f6a02fb97edb2c55
yaelsand <yaelangel@gmail.com>
2017-04-02 21:41:39 +0300

    ex3

--- app/src/main/java/yael/todolistmanager/MainActivity.java
+++ app/src/main/java/yael/todolistmanager/MainActivity.java
@@ -1,21 +1,31 @@
 package yael.todolistmanager;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
 
 public class MainActivity extends Activity {
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
     private ListView lvItems;
+    private Calendar calendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,45 +34,106 @@ public class MainActivity extends Activity {
 
         lvItems = (ListView) findViewById(R.id.lvItems);
         items = new ArrayList<String>();
-        itemsAdapter = new AlternateColorAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        itemsAdapter = new yael.todolistmanager.AlternateColorAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-        items.add("First Item");
-        items.add("Second Item");
         // Setup remove listener method call
         setupListViewListener();
     }
 
     public void onAddItem(View v) {
-        EditText newItem = (EditText) findViewById(R.id.newItem);
-        String itemText = newItem.getText().toString();
-        itemsAdapter.add(itemText);
-        newItem.setText("");
+        // open a dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add New Item");
+
+        // Set up the input
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        final EditText newItem = new EditText(this);
+        newItem.setHint("Add New Item");
+        layout.addView(newItem);
+
+        final EditText newDate = new EditText(this);
+        newDate.setHint("Pick a Date");
+
+        // Set up the date picker
+        final DatePickerDialog.OnDateSetListener datePicker = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                String myFormat = "MM/dd/yy";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+                newDate.setText(sdf.format(calendar.getTime()));
+            }
+        };
+
+        newDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this, datePicker,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH))
+                        .show();
+            }
+        });
+        layout.addView(newDate);
+        builder.setView(layout);
+
+        // Set up the buttons
+        builder.setPositiveButton("Add Item", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String itemText = newItem.getText().toString() + "\n" + newDate.getText().toString();
+                itemsAdapter.add(itemText);
+                newItem.setText("");
+                newDate.setText("");
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 
     // Attaches a long click listener to the listview
     private void setupListViewListener() {
         lvItems.setOnItemLongClickListener(
-            new AdapterView.OnItemLongClickListener() {
-                @Override
-                public boolean onItemLongClick(AdapterView<?> adapter,
-                                               View item, final int pos, long id) {
-                    // open a dialog
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle(lvItems.getItemAtPosition(pos).toString())
-                            .setMessage("Are you sure you want to delete this item?")
-                            .setPositiveButton("delete", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    // Remove the item within array at position
-                                    items.remove(pos);
-                                    // Refresh the adapter
-                                    itemsAdapter.notifyDataSetChanged();
-                                }
-                            });
-                    AlertDialog alert = builder.create();
-                    alert.show();
-                    return true;
-                }
-            });
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, final int pos, long id) {
+                        // add option to call
+                        if (lvItems.getItemAtPosition(pos).toString().startsWith("call")) {
+                            Intent intent = new Intent(Intent.ACTION_DIAL);
+                            String phoneNum = lvItems.getItemAtPosition(pos).toString().split("\n", 2)[0];
+                            phoneNum = phoneNum.replaceAll("[^0-9]", "");
+                            intent.setData(Uri.parse("tel:" + phoneNum));
+                            startActivity(intent);
+                        }
+                        // open a dialog
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle(lvItems.getItemAtPosition(pos).toString())
+                                .setMessage("What would you like to do?")
+                                .setPositiveButton("Delete this item", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        // Remove the item within array at position
+                                        items.remove(pos);
+                                        // Refresh the adapter
+                                        itemsAdapter.notifyDataSetChanged();
+                                    }
+                                });
+                        AlertDialog alert = builder.create();
+                        alert.show();
+                        return true;
+                    }
+                });
     }
 
 }
